157139ca0fb51ffb970a1709602e106600348882
Rish0059 <24mb0059@iitism.ac.in>
2025-08-03 08:39:30 +0530

    Add files via upload

---
+++ appy.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    # Remove the CWD from sys.path while we load stuff.
+    # This is added back by InteractiveShellApp.init_path()
+    if sys.path[0] == "":
+        del sys.path[0]
+
+    from ipykernel import kernelapp as app
+
+    app.launch_new_instance()
